4124ffd8f886013af2f83045ed7fa8f8522a7ca7
Aryan <aryangiroud@gmail.com>
2022-12-12 22:21:18 +0530

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import SettingsScreen from './screens/SettingsScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import NotificationScreen from './screens/NotificationScreen';
+import UserScreen from './screens/UserScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+function BottomNavTabs() {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name='Home' component={HomeScreen}
+				options={{
+					tabBarLabel: 'Home',
+					tabBarIcon: ({color, size}) => (
+						<MaterialCommunityIcons name='home' color={color} size={size} />
+					),
+				}} />
+			<Tab.Screen name='Notification' component={NotificationScreen}
+				options={{
+					tabBarLabel: 'Notification',
+					tabBarIcon: ({color, size}) => (
+						<MaterialCommunityIcons name='bell' color={color} size={size} />
+					),
+				}} />
+			<Tab.Screen name='Settings' component={SettingsScreen}
+				options={{
+					tabBarLabel: 'Settings',
+					tabBarIcon: ({color, size}) => (
+						<MaterialCommunityIcons name='cog' color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen name='User' component={UserScreen}
+				options={{
+					tabBarLabel: 'User',
+					tabBarIcon: ({color, size}) => (
+						<MaterialCommunityIcons name='account' color={color} size={size} />
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+	return (
+		<NavigationContainer>
+			<BottomNavTabs />
+		</NavigationContainer>
+	);
+}
+
+export default App;
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+
+export default function SettingsScreen() {
+	return (
+		<View>
+			<Text style={tw`text-md text-green-500`}>Settings</Text>
+		</View>
+	);
+}
